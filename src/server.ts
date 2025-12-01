f8a1b7b8c5b6853a797e2da46a4588343082a68d
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-12-01 16:52:24 +0600

    practice: post method in postgreSQL

--- src/server.ts
+++ src/server.ts
@@ -29,15 +29,6 @@ const initDB = async () => {
         )
         `);
 
-  //     id SERIAL PRIMARY KEY,
-  // user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  // title VARCHAR(200) NOT NULL,
-  // description TEXT,
-  // completed BOOLEAN DEFAULT false,
-  // due_date DATE,
-  // created_at TIMESTAMP DEFAULT NOW(),
-  // updated_at TIMESTAMP DEFAULT NOW()
-
   await pool.query(`
     CREATE TABLE IF NOT EXISTS todos(
     id SERIAL PRIMARY KEY,
@@ -58,8 +49,26 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next level developer!");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name,email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    // console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      messsage: "Data Inserted Successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      messsage: err.message,
+    });
+  }
 
   res.status(201).json({
     success: true,
